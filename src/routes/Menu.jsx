c7ea2bc10b55140d431e3cf28765d5ac2068f0f9
MariaRipley <roblesb.maria@gmail.com>
2023-12-01 13:14:40 +0100

    Add toggle menu for mobile version

--- src/routes/Menu.jsx
+++ src/routes/Menu.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/Menu.scss";
 
 function Menu() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="menu">
-      <ion-icon name="menu-outline"></ion-icon>
-      <nav className="menu__nav">
-        <ul className="menu__list">
+      <ion-icon
+        name="menu-outline"
+        className={`${showMenu ? "open-menu" : ""}`}
+        onClick={toggleMenu}
+      ></ion-icon>
+      <nav className={`menu__nav ${showMenu ? "show" : ""}`}>
+        <ul className={`menu__list ${showMenu ? "vertical" : ""}`}>
           <li className="menu__item">
             <NavLink className={"menu__link"} to={"/"}>
               Home
--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -3,10 +3,10 @@ import "../styles/Portfolio.scss";
 
 function Portfolio() {
   return (
-    <>
+    <div className="portfolio">
       <Header />
       <h1>PORTFOLIO</h1>
-    </>
+    </div>
   );
 }
 
